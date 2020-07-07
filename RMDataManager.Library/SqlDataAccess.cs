c3a845267fe2bb4809bcf91d185d78cadb4a758d
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-07-07 22:16:19 +0630

    Get Connection String
    Add ConfiguratioManager reference and make GetConnectionString to get default connection with DB

--- RMDataManager.Library/SqlDataAccess.cs
+++ RMDataManager.Library/SqlDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,9 @@ namespace RMDataManager.Library
 {
     public class SqlDataAccess
     {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
     }
 }
