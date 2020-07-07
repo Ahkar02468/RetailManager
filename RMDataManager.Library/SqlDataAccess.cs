314287c371f70161ca7dc26d2d31824c9d3db625
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-07-07 23:17:52 +0630

    Load data
    Create Load<T,U> to get data from DB

--- RMDataManager.Library/SqlDataAccess.cs
+++ RMDataManager.Library/SqlDataAccess.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,16 @@ namespace RMDataManager.Library
         public string GetConnectionString(string name)
         {
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure,U parameters,string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure).ToList();
+                return rows;
+            }
         }
     }
 }
