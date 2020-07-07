c0150051b8609a2081cd9148f2a783bdf59764f2
Ahkar Shwebaw <arkarshwebaw@gmail.com>
2020-07-07 23:20:48 +0630

    Save Data to DB
    Create SaveData<T> to save data to DB

--- RMDataManager.Library/SqlDataAccess.cs
+++ RMDataManager.Library/SqlDataAccess.cs
@@ -26,5 +26,14 @@ namespace RMDataManager.Library
                 return rows;
             }
         }
+
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
     }
 }
